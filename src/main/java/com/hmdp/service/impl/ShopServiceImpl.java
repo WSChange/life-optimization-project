710e3f25fdf21a5f918276d471e091c5c27ca31a
WSChange <1578632568@qq.com>
2026-01-08 14:50:20 +0800

    通过互斥锁解决缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -32,19 +33,95 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     private StringRedisTemplate stringRedisTemplate;
     @Override
     public Result queryById(Long id) {
+        // 缓存穿透
+        // Shop shop = queryWithPassThrough(id);
+        // 互斥锁解决缓存击穿
+        Shop shop = queryWithMutex(id);
+        if (shop == null){
+            return Result.fail("店铺不存在！");
+        }
+        //7. 返回
+        return Result.ok(shop);
+    }
+
+    public Shop queryWithMutex(Long id){
+        String key = CACHE_SHOP_KEY + id;
+        // 1. 从redis查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2. 判断是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            //3. 存在直接返回，将字符串反序列化为java对象
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+        // 判断命中的是否是空值
+        if (shopJson != null){
+            // 返回错误信息
+            return null;
+        }
+
+        // 4. 实现缓存重建
+        // 4.1 获取互斥锁
+        String lockKey = "lock:shop:" + id;
+        Shop shop = null;
+        try {
+            // 4.2 判断是否获取成功
+            boolean isLock = tryLock(lockKey);
+
+            if (!isLock){
+                // 4.3 失败，则休眠并重试
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+
+            // 4.4 成功则根据id查询数据库，这里获取锁成功应再次检测redis缓存是否存在，做DoubleCheck。如果存在则无需重建缓存。
+            shop = getById(id);
+
+            // 由于本地查询数据库较快，这里模拟重建延时
+            Thread.sleep(200);
+
+            // 5. 不存在则返回错误
+            if (shop == null){
+                // 将空值写入redis，并设置有效期
+                stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
+                // 返回错误信息
+                return null;
+            }
+            // 6. 存在，写入redis，这里设置过期时间以执行超期剔除的缓存更新策略
+            stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // 7. 释放互斥锁
+            unLock(lockKey);
+        }
+        // 8. 返回
+        return shop;
+    }
+
+    // 添加互斥锁，采用String类型中的SETNX方法实现
+    private boolean tryLock(String key){
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key,"1",10,TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+    // 释放互斥锁，本质是使用del命令删除key
+    private void unLock(String key){
+        stringRedisTemplate.delete(key);
+    }
+
+    // 保存缓存穿透代码
+    public Shop queryByPassThrough(Long id){
         String key = CACHE_SHOP_KEY + id;
         //1. 从redis查询商铺缓存
         String shopJson = stringRedisTemplate.opsForValue().get(key);
         //2. 判断是否存在
         if (StrUtil.isNotBlank(shopJson)) {
             //3. 存在直接返回，将字符串反序列化为java对象
-            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return JSONUtil.toBean(shopJson, Shop.class);
         }
         // 判断命中的是否是空值
         if (shopJson != null){
             // 返回错误信息
-            return Result.fail("店铺信息不存在");
+            return null;
         }
         //4. 不存在则根据id查询数据库
         Shop shop = getById(id);
@@ -52,12 +129,12 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         if (shop == null){
             // 将空值写入redis
             stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
-            return Result.fail("店铺不存在！");
+            return null;
         }
         //6. 存在，写入redis，这里设置过期时间以执行超期剔除的缓存更新策略
         stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
         //7. 返回
-        return Result.ok(shop);
+        return shop;
     }
 
     @Override
